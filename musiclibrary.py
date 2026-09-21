4ad025e0fcf5fc0b2fdd8514b1f3d3b37c372365
FarhanAlam-hub <justinfarhan786@gmail.com>
2026-09-21 23:45:29 +0530

    add some music

--- musiclibrary.py
+++ musiclibrary.py
@@ -1,6 +1,7 @@
 music = {
     "baby" : "https://www.youtube.com/watch?v=kffacxfA7G4",
     "despacito" : "https://youtu.be/kJQP7kiw5Fk?si=4HSwgXA9LuRrIhp4",
-    "gumaan" : "https://youtu.be/jIQ0Dx-4peE?si=KY9lv6CHveN6dkMJ",
-    "home" : "https://www.youtube.com/watch?v=ZWG9iXnrDfI"
+    "chanel" : "https://youtu.be/xiZUf98A1Ts",
+    "home" : "https://www.youtube.com/watch?v=ZWG9iXnrDfI",
+    "perfect" : "https://youtu.be/2Vv-BfVoq4g"
 }
